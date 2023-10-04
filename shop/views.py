69df978e8ca813ee887258ac07099beb40775eac
Frankva <9905928@gmail.com>
2023-10-04 08:56:59 +0200

    style, shop, image

--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-
+from .models import Product
 
 def index(request):
     context = dict()
+    context['products'] = Product.objects.all().values()
+    def add(product):
+        product['image'] =  product['image'][11:] 
+        #product['image'] = 'shop/images/' + product['image']
+        return product
+    context['products'] = list(map(add, context['products']))
     return render(request, 'shop/index.html', context)
 
 def images(request, name):
     context = dict()
-    context['src'] = 'shop/images/'+ name
+    context['src'] = 'shop/images/' + name
     return render(request, 'shop/images.html', context)
